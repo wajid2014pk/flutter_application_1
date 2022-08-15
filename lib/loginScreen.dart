6ed1b99b5a517ab8f92496a48d99a6bf08fa61eb
wajid2014pk <wajid2014pk@gmail.com>
2022-08-16 00:18:12 +0500

    Day 4 completed

--- lib/loginScreen.dart
+++ lib/loginScreen.dart
@@ -3,16 +3,51 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Login Screen")),
-      body: Center(
-        child: Text(
-          "LOGIN WITH FLUTTER",
-          style: TextStyle(
-              fontSize: 30, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-      ),
-      //drawer: Drawer(backgroundColor: Colors.yellowAccent),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/login_image.png',
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text("Welcome to Login Screen",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 5.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter your username ",
+                        labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter your Password ",
+                        labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("HI wajid");
+                    },
+                    child: Text("Login"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_application_1/home_page.dart';
 import 'package:flutter_application_1/loginScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,7 +15,9 @@ class MyApp extends StatelessWidget {
       title: "Flutter App",
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(
+          primarySwatch: Colors.green,
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(
           brightness: Brightness.light, primarySwatch: Colors.deepPurple),
       routes: {
